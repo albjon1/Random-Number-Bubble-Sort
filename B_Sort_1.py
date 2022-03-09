a0d6ce47bcf1f819b544bf5cf63e1df1fdf11c88
Albjon Husha <81479352+albjon1@users.noreply.github.com>
2022-03-09 21:59:33 +0000

    Update B_Sort_1.py

--- B_Sort_1.py
+++ B_Sort_1.py
@@ -4,7 +4,7 @@ import random
 # Starting runtime
 start_time = time.time()
 
-
+# Bubble Sort Function
 def b_sort(sort):
 
     swap = True
@@ -15,14 +15,15 @@ def b_sort(sort):
                 sort[x], sort[x + 1] = sort[x + 1], sort[x]
                 swap = True
 
-
+# Creating randomly generated list of 1000 integers
 randomList = []
 
 for i in range(1, 1001):
     nums = random.randint(1, 100001)
     randomList.append(nums)
+
 print('Generated list = ', randomList)
-b_sort(randomList)
+b_sort(randomList) # Running the algorithm on the list
 print('Sorted = ', randomList)
 
 # Ending Runtime
